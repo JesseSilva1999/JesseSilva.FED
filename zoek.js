d8e6add4e6907c2d96fbac42c9818010a1863a5e
JesseSilva1999 <60040536+JesseSilva1999@users.noreply.github.com>
2021-03-31 01:03:24 +0200

    Create zoek.js

---
+++ zoek.js
@@ -0,0 +1,17 @@
+
+
+function myFunction() {
+  var ingeklapt = document.getElementById("ingeklapt");
+  var meer = document.getElementById("meer");
+  var buttontext = document.getElementById("button");
+
+  if (ingeklapt.style.display === "none") {
+    ingeklapt.style.display = "inline";
+    buttontext.innerHTML = "Bekijk meer";
+    meer.style.display = "none";
+  } else {
+    ingeklapt.style.display = "none";
+    buttontext.innerHTML = "bekijk minder";
+    meer.style.display = "inline";
+  }
+}
